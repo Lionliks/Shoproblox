cfccf35d1e4140af62d474de5856f934922410a7
Lionliks <n4105806@gmail.com>
2025-10-03 23:51:33 +0300

    Add files via upload

---
+++ states_groups.py
@@ -0,0 +1,29 @@
+from aiogram.dispatcher.filters.state import StatesGroup, State
+
+
+class CreateOrEditProfileStatesGroup(StatesGroup):
+    height = State()
+    weight = State()
+    gender = State()
+    age = State()
+    level_of_activities = State()
+
+class SearchProductsStatesGroup(StatesGroup):
+    q = State()
+    show_result = State()
+
+class SearchRecipesStatesGroup(StatesGroup):
+    q = State()
+    show_result = State()
+
+class AddToDiaryStatesGroup(StatesGroup):
+    get_group = State()
+    get_volume = State()
+    get_date = State()
+
+class MailingStatesGroup(StatesGroup):
+    get_message = State()
+    get_confirmation = State()
+
+class UnBlockUserStatesGroup(StatesGroup):
+    get_username = State()
